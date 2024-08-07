66e1fa5e127f69bcdfda0f0c94dc3bbca199db75
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2024-08-07 12:36:30 -0600

    Update hangman.cpp

--- hangman.cpp
+++ hangman.cpp
@@ -43,15 +43,39 @@ void draw_man(int wrong_ans) {
     }
     
     std::cout << "         _____\n";
-    std::cout << "         |    |\n";
+    std::cout << "        |     |\n";
     std::cout << "        " << l_head << r_head <<"    |\n";
     std::cout << "       " << l_arm << body << r_arm << "    |\n";
     std::cout << "        " << l_leg << r_leg << "    |\n";
-    std::cout << "        ______|______\n";
+    std::cout << "        ______|______\n\n";
 }
 
+int fun_display_word(std::string user_guess, int wrong_ans) {
+    std::string letter_1 = "_ ";
+    std::string letter_2 = "_ ";
+    std::string letter_3 = "_ ";
+    std::string letter_4 = "_ ";
+    std::string letter_5 = "_ ";
+    
+    std::string display_word = letter_1 + letter_2 + letter_3 + letter_4 + letter_5;
+    
+    std::cout << "          " << display_word << "\n\n";
+    
+    if(user_guess != display_word) {
+        wrong_ans++;
+        std::cout << "Your guess is incorrect.\n";
+    }
+    else {
+        std::cout << "Your guess is correct.\n";
+    }
+    
+    return wrong_ans;
+}
 
 int main() {
+    int wrong_ans = 0;
+    draw_man(wrong_ans);
+    fun_display_word("_ _ _ _ _ ", wrong_ans);
     
     return 0;
 }
